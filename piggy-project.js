3e11861aa98b9abb790da17594f891fbe148c2b5
Hayrpum <hayrpum1@gmail.com>
2021-07-27 13:40:56 +0300

    NextFix

--- piggy-project.js
+++ piggy-project.js
@@ -1,33 +1,106 @@
-const sum_input = document.querySelector('#sum')
-const term_input = document.querySelector('#term')
-const startSum = document.querySelector('#start_sum')
-const calc = document.querySelector('.calc')
-const newTask = document.querySelector('.task')
 const container = document.querySelector('.container')
-const deleteButton = document.querySelector('.btn-remove')
-let area = document.querySelector('.output')
-let form = document.querySelector('.form')
-let formContent = form.innerHTML
+
+let ID = 0;
+function generateId() {
+    ID++;
+    return ID;
+}
+
+let formContent = `
+<div class="section">
+    <div class="task-section">
+        <div class="task">
+            <p class="title-p">Task</p>
+        </div>
+        <div class="input-list">
+            <input id="purpose" type="text" class="input" placeholder="Цель">
+            <input id="sum" type="number" class="input" placeholder="Требуемая сумма">
+            <input id="term" type="date" class="input" placeholder="Срок">
+            <input id="start-sum" type="number" class="input" placeholder="Стартовая сумма">
+            <input id="percent" type="number" class="input" placeholder="Процент">
+        </div class="payment">
+        <p class="p-payment">monthly payment:</p>
+        <div class="result">
+
+        </div>
+        <div class="btn-group">
+            <button class="btn-calculate">
+                Calculate
+            </button>
+            <button class="btn-delete">
+                Delete
+            </button>
+        </div>
+
+    </div>
+</div>
+`
 
 
+creatNewForm()
 let dataArray = []
 
-newTask.addEventListener('click', () => {
-    const object = document.createElement('object')
-    dataArray.push(object)
-    console.log(dataArray);
-    creatNewForm()
-})
 
-
-calc.addEventListener('click', (event) => {
-    result = (sum_input.value - startSum.value) / term_input.value
-    area.innerHTML = result
-})
 
 function creatNewForm() {
     const newElementDiv = document.createElement('div')
+    newElementDiv.id = generateId()
+
     newElementDiv.classList.add('.form')
     newElementDiv.innerHTML = formContent
     container.append(newElementDiv)
+
+    const deleteButton = newElementDiv.querySelector('.btn-delete')
+    deleteButton.addEventListener('click', () => {
+        console.log(container.childElementCount)
+        if (container.childElementCount > 1) {
+            console.log(deleteButton);
+            newElementDiv.remove()
+        }
+    })
+
+    const calc = newElementDiv.querySelector('.btn-calculate')
+    const newTask = newElementDiv.querySelector('.task')
+
+    newTask.addEventListener('click', () => {
+
+        creatNewForm()
+    })
+
+
+    calc.addEventListener('click', (event) => {
+        let area = newElementDiv.querySelector('.result')
+        const sum_input = newElementDiv.querySelector('#sum')
+        const term_input = newElementDiv.querySelector('#term')
+        const startSum = newElementDiv.querySelector('#start-sum')
+        const purpose = newElementDiv.querySelector('#purpose')
+        const percent = newElementDiv.querySelector('#percent')
+
+        let object = dataArray.find((el) => {
+            return el.id === newElementDiv.id
+        });
+        if (!object) {
+            object = {
+                id: newElementDiv.id,
+                Goal: purpose.value,
+                requiredAmount: sum_input.value,
+                Term: term_input.value,
+                StartSum: startSum.value,
+                Precent: percent.value
+            }
+            dataArray.push(object)
+        } else {
+            object.Goal = purpose.value;
+            object.requiredAmount = sum_input.value
+            object.Term = term_input.value
+            object.StartSum = startSum.value
+            object.Precent = percent.value
+        }
+
+        console.log(dataArray);
+
+        let result = (sum_input.value - startSum.value) / term_input.value
+        area.innerHTML = result
+    })
+
 }
