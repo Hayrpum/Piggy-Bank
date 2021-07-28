58eeeb8ace1cc892ca5deb4c53e54db7172b99b4
Umar <umachapanov@gmail.com>
2021-07-28 12:02:58 +0300

    apply diff from main THAT SHOULD NEVER BE IN MAIN

--- piggy-project.js
+++ piggy-project.js
@@ -1,11 +1,9 @@
 const container = document.querySelector('.container')
-
 let ID = 0;
 function generateId() {
     ID++;
     return ID;
 }
-
 let formContent = `
         <div class="section">
             <div class="task-section">
@@ -65,25 +63,20 @@ function creatNewForm() {
             console.log(deleteButton);
             newElementDiv.remove()
         }
+        else {
+            sum_input.value = ''
+            term_input.value = ''
+            startSum.value = ''
+            purpose.value = ''
+            percent.value = ''
+        }
     })
-
     const calc = newElementDiv.querySelector('.btn-calculate')
     const newTask = newElementDiv.querySelector('.task')
-
     newTask.addEventListener('click', () => {
-
         creatNewForm()
     })
-
-
-    calc.addEventListener('click', (event) => {
-        let area = newElementDiv.querySelector('.result')
-        const sum_input = newElementDiv.querySelector('#sum')
-        const term_input = newElementDiv.querySelector('#term')
-        const startSum = newElementDiv.querySelector('#start-sum')
-        const purpose = newElementDiv.querySelector('#purpose')
-        const percent = newElementDiv.querySelector('#percent')
-
+    function algoritmCalc() {
         let object = dataArray.find((el) => {
             return el.id === newElementDiv.id
         });
@@ -94,7 +87,8 @@ function creatNewForm() {
                 requiredAmount: sum_input.value,
                 Term: term_input.value,
                 StartSum: startSum.value,
-                Precent: percent.value
+                Precent: percent.value,
+                MonthlyPayment: area.value
             }
             dataArray.push(object)
         } else {
@@ -103,12 +97,55 @@ function creatNewForm() {
             object.Term = term_input.value
             object.StartSum = startSum.value
             object.Precent = percent.value
+            object.MonthlyPayment = area.value
         }
-
-        console.log(dataArray);
-
-        let result = (sum_input.value - startSum.value) / term_input.value
-        area.innerHTML = result
+        function calculateResult() {
+            let sumPer = 0
+            for (i = 1; i < Number(term_input.value) + 1; i++) {
+                sumPer = sumPer + (1 + Number(percent.value) / 100) ** i
+                let payment = (Number(sum_input.value) - ((1 + Number(percent.value) / 100) ** Number(term_input.value)) * Number(startSum.value)) / sumPer
+                console.log(payment)
+                if (payment < 0) {
+                    payment = 0
+                }
+                area.innerHTML = payment.toFixed(2)
+            }
+        }
+        calculateResult()
+    }
+    calc.addEventListener('click', (event) => {
+        algoritmCalc()
     })
-
+    sum_input.addEventListener('input', () => {
+        
+        algoritmCalc()
+    })
+    startSum.addEventListener('input', () => {
+        
+        algoritmCalc()
+    })
+    term_input.addEventListener('input', () => {
+        algoritmCalc()
+    })
+    percent.addEventListener('input', () => {
+        algoritmCalc()
+    })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
