1538dc6a7962e2df6276ace538734cbf5a397538
Hayrpum <hayrpum1@gmail.com>
2021-07-28 12:10:07 +0300

    TeamLead commit

--- piggy-project.js
+++ piggy-project.js
@@ -7,33 +7,40 @@ function generateId() {
 }
 
 let formContent = `
-<div class="section">
-    <div class="task-section">
-        <div class="task">
-            <p class="title-p">Task</p>
+        <div class="section">
+            <div class="task-section">
+                <div class="foot">
+                    <div class="logo">
+                        <img class="img-logo" src="./img/Clip1.jpg" alt="logo"></img>
+                        <p id="logo-text">moneybox</p>
+                    </div>
+                    <div class="task">
+                        <p class="title-p">new dream</p>
+                    </div>
+                </div>
+                <div class="input-list">
+                    <input id="purpose" type="text" class="input" placeholder="purpose">
+                    <input id="sum" type="number" class="input" placeholder="required amount">
+                    <input id="term" type="number" class="input" placeholder="term">
+                    <input id="start-sum" type="number" class="input" placeholder="initial amount">
+                    <input id="percent" type="number" class="input" placeholder="percent">
+                </div>
+                <div class="payment">
+                    <p class="p-payment">monthly payment :</p>
+                </div>
+                <div class="result">
+                   
+                </div>
+                <div class="btn-group">
+                    <button class="btn-calculate">
+                        Calculate
+                    </button>
+                    <button class="btn-delete">
+                        Delete
+                    </button>
+                </div>
+            </div>
         </div>
-        <div class="input-list">
-            <input id="purpose" type="text" class="input" placeholder="Цель">
-            <input id="sum" type="number" class="input" placeholder="Требуемая сумма">
-            <input id="term" type="date" class="input" placeholder="Срок">
-            <input id="start-sum" type="number" class="input" placeholder="Стартовая сумма">
-            <input id="percent" type="number" class="input" placeholder="Процент">
-        </div class="payment">
-        <p class="p-payment">monthly payment:</p>
-        <div class="result">
-
-        </div>
-        <div class="btn-group">
-            <button class="btn-calculate">
-                Calculate
-            </button>
-            <button class="btn-delete">
-                Delete
-            </button>
-        </div>
-
-    </div>
-</div>
 `
 
 
@@ -49,13 +56,25 @@ function creatNewForm() {
     newElementDiv.classList.add('.form')
     newElementDiv.innerHTML = formContent
     container.append(newElementDiv)
+    let area = newElementDiv.querySelector('.result')
+    const sum_input = newElementDiv.querySelector('#sum')
+    const term_input = newElementDiv.querySelector('#term')
+    const startSum = newElementDiv.querySelector('#start-sum')
+    const purpose = newElementDiv.querySelector('#purpose')
+    const percent = newElementDiv.querySelector('#percent')
 
     const deleteButton = newElementDiv.querySelector('.btn-delete')
     deleteButton.addEventListener('click', () => {
-        console.log(container.childElementCount)
         if (container.childElementCount > 1) {
             console.log(deleteButton);
             newElementDiv.remove()
+        }
+        else {
+            sum_input.value = ''
+            term_input.value = ''
+            startSum.value = ''
+            purpose.value = ''
+            percent.value = ''
         }
     })
 
@@ -63,19 +82,10 @@ function creatNewForm() {
     const newTask = newElementDiv.querySelector('.task')
 
     newTask.addEventListener('click', () => {
-
         creatNewForm()
     })
 
-
-    calc.addEventListener('click', (event) => {
-        let area = newElementDiv.querySelector('.result')
-        const sum_input = newElementDiv.querySelector('#sum')
-        const term_input = newElementDiv.querySelector('#term')
-        const startSum = newElementDiv.querySelector('#start-sum')
-        const purpose = newElementDiv.querySelector('#purpose')
-        const percent = newElementDiv.querySelector('#percent')
-
+    function algoritmCalc() {
         let object = dataArray.find((el) => {
             return el.id === newElementDiv.id
         });
@@ -86,21 +96,51 @@ function creatNewForm() {
                 requiredAmount: sum_input.value,
                 Term: term_input.value,
                 StartSum: startSum.value,
-                Precent: percent.value
+                Precent: percent.value,
+                MonthlyPayment: area.value
             }
             dataArray.push(object)
+
         } else {
             object.Goal = purpose.value;
             object.requiredAmount = sum_input.value
             object.Term = term_input.value
             object.StartSum = startSum.value
             object.Precent = percent.value
+            object.MonthlyPayment = area.value
         }
+        function calculateResult() {
+            let sumPer = 0
+            for (i = 1; i < Number(term_input.value) + 1; i++) {
+                sumPer = sumPer + (1 + Number(percent.value) / 100) ** i
+                let payment = (Number(sum_input.value) - ((1 + Number(percent.value) / 100) ** Number(term_input.value)) * Number(startSum.value)) / sumPer
+                console.log(payment)
+                if (payment < 0) {
+                    payment = 0
+                }
+                area.innerHTML = payment.toFixed(2)
+            }
+        }
+        calculateResult()
+    }
 
-        console.log(dataArray);
 
-        let result = (sum_input.value - startSum.value) / term_input.value
-        area.innerHTML = result
+    calc.addEventListener('click', (event) => {
+        algoritmCalc()
     })
+    sum_input.addEventListener('input', () => {
+        algoritmCalc()
+    })
+    startSum.addEventListener('input', () => {
+        algoritmCalc()
+    })
+
+    term_input.addEventListener('input', () => {
+        algoritmCalc()
+    })
+    percent.addEventListener('input', () => {
+        algoritmCalc()
+    })
+
 
 }
